f6f293859ef40ab992366bdcb6ab1d81242ae5ad
Gavin Ching <gavinchingy@gmail.com>
2013-05-16 17:47:24 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aiesecdb::Application.config.secret_token = '3a8a48882ed5e5e0e1303afd77ae51ebc2c5063e927abe979987fa533be363635cf019bac77cb9da221b38ccde6d586528e55f6ab324f703bdcd0c75a68b3b45'
