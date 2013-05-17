7f7546da0e4a06a198923b4c3d4e036d2b25b86f
Gavin Ching <gavinchingy@gmail.com>
2013-05-17 18:10:20 -0400

    Made default creation of user to be of user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   rolify
+  after_create :set_default_roles
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -9,5 +10,9 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  
+
+private 
+  def set_default_roles
+    add_role(:user)
+  end
 end
